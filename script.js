e8c7ba91987c1eb47f5c336a64acf5712cee65c3
Aansu <aansunirupamajacob94@gmail.com>
2021-07-04 19:42:57 +0530

    Calculator

---
+++ script.js
@@ -0,0 +1,12 @@
+// alert("hello")
+// function displayNumber(num){
+//     inpt.value+=num;
+// }
+
+let displayNumber=(num)=>inpt.value+=num;
+
+let evaluateExpression=()=>inpt.value=eval(inpt.value);
+
+let clearBox=()=>inpt.value="";
+    
+let removeNumber=()=>inpt.value=inpt.value.slice(0,-1);
